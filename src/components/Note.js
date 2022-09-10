5539f211896026fdc6d0c9c32002de139aa2813e
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-09-10 13:15:56 +0200

    Added delete functionality to the Note component

--- src/components/Note.js
+++ src/components/Note.js
@@ -5,13 +5,13 @@ import { MdDeleteForever } from 'react-icons/md'
 import '../css/Note.css';
 
 // Creating the Note component
-const Note = ({ id, text, date}) => {
+const Note = ({ id, text, date, handleDeleteNote }) => {
     return (
         <div className="note" >
             <span>{text}</span>
             <div className="note-footer">
                 <small>{date}</small>
-                <MdDeleteForever className='delete-icon' size='1.3em' />
+                <MdDeleteForever onClick={() => {handleDeleteNote(id)}} className='delete-icon' size='1.3em' />
             </div>
         </div>
     )
--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -8,10 +8,10 @@ import Note from './Note';
 import AddNote from './AddNote';
 
 // Creating the NotesList component
-const NotesList = ({ notes, handleAddNote }) =>{
+const NotesList = ({ notes, handleAddNote, handleDeleteNote }) =>{
     return (
         <div className='notes-list'>
-            {notes.map(note => <Note key={note.id} id={note.id} text={note.text} date={note.date} />)}
+            {notes.map(note => <Note key={note.id} id={note.id} text={note.text} date={note.date} handleDeleteNote={handleDeleteNote} />)}
             <AddNote handleAddNote={handleAddNote} />
         </div>
     )
