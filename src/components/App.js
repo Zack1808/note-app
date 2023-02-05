1ce66dd19a1d9394c59563d8700ca242f20a4a3c
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-02-05 09:58:01 +0100

    Modified the styling and added favicon

--- src/components/App.js
+++ src/components/App.js
@@ -1,58 +1,65 @@
-import React, { useEffect, useState } from 'react';
-import { nanoid as nanoId } from 'nanoid'
+import React, { useEffect, useState } from "react";
+import { nanoid as nanoId } from "nanoid";
 
 // Importing the style file
-import '../css/App.css';
+import "../css/App.css";
 
 // Improting the costume made components
-import NotesList from './NotesList';
-import Search from './Search';
-import Header from './Header';
+import NotesList from "./NotesList";
+import Search from "./Search";
+import Header from "./Header";
 
 // Creating the App component
-const App = () =>{
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [darkMode, setDarkMode] = useState(false);
 
-    const [notes, setNotes] = useState([]);
-    const [search, setSearch] = useState("");
-    const [darkMode, setDarkMode] = useState(false);
+  useEffect(() => {
+    const savedNotes = JSON.parse(localStorage.getItem("react-notes-app-data"));
+    const darkModeStatus = JSON.parse(
+      localStorage.getItem("react-notes-app-dark")
+    );
+    if (savedNotes) setNotes(savedNotes);
+    setDarkMode(darkModeStatus);
+  }, []);
 
-    useEffect(() => {
-        const savedNotes = JSON.parse(localStorage.getItem('react-notes-app-data'));
-        const darkModeStatus = JSON.parse(localStorage.getItem('react-notes-app-dark'));
-        if(savedNotes) setNotes(savedNotes)
-        setDarkMode(darkModeStatus)
-    }, [])
+  useEffect(() => {
+    localStorage.setItem("react-notes-app-data", JSON.stringify(notes));
+    localStorage.setItem("react-notes-app-dark", JSON.stringify(darkMode));
+  }, [notes, darkMode]);
 
-    useEffect(() => {
-        localStorage.setItem('react-notes-app-data', JSON.stringify(notes));
-        localStorage.setItem('react-notes-app-dark', JSON.stringify(darkMode));
-    }, [notes, darkMode]);
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoId(),
+      text: text,
+      date: date.toLocaleDateString(),
+    };
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
 
-    const addNote = (text) => {
-        const date = new Date();
-        const newNote = {
-            id: nanoId(),
-            text: text,
-            date: date.toLocaleDateString(),
-        }
-        const newNotes = [...notes, newNote];
-        setNotes(newNotes)
-    }
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
 
-    const deleteNote = (id) => {
-        const newNotes = notes.filter((note) => note.id != id);
-        setNotes(newNotes)
-    }
-
-    return(
-        <div className={`${darkMode && "dark-mode"}`}>
-            <div className="container">
-                <Header handleToggleDarkMode={setDarkMode} />
-                <Search search={search} setSearch={setSearch} />
-                <NotesList notes={notes.filter(note => note.text.toLowerCase().includes(search.toLowerCase()))} handleAddNote={addNote} handleDeleteNote={deleteNote} />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={`background ${darkMode && "dark-mode"}`}>
+      <div className="container">
+        <Header handleToggleDarkMode={setDarkMode} />
+        <Search search={search} setSearch={setSearch} />
+        <NotesList
+          notes={notes.filter((note) =>
+            note.text.toLowerCase().includes(search.toLowerCase())
+          )}
+          handleAddNote={addNote}
+          handleDeleteNote={deleteNote}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default App;
